f439b67d73dc8431c30880d3e2785e6130923dd9
Rocke215 <rocke1995@gmail.com>
2022-05-12 20:53:54 +0300

    add users unittests

--- backend/tests/test_example.py
+++
@@ -1,9 +0,0 @@
-# Каждый логический набор тестов — это класс,
-# который наследуется от базового класса TestCase
-from django.test import TestCase
-
-
-class Test(TestCase):
-    def test_example(self):
-        # пишем тест тут
-        ...
